db1eb26745e4cf243bbae3b47f2496faa3f19c34
Santosh <spant671@gmail.com>
2026-04-18 10:47:42 +0530

    commited

--- variables.java
+++ variables.java
@@ -18,6 +18,47 @@ public class variables {
         System.out.println("weight " +  weight);
         System.out.println("WEIGHT " +  WEIGHT);
 
-        
+        //Starting Letter
+        int marks =10;
+        int MARKS=11;
+        int _marks =12;
+        int $marks =13;
+        System.out.println(marks);
+        System.out.println(MARKS);
+        System.out.println(_marks);
+        System.out.println($marks);
+
+        //Subsequent Letters
+        int height5 =21;
+        int height_two= 33;
+        int height$12jk=44;
+        int height_=22;
+        int height$= 23;
+
+        System.out.println(height5);
+        System.out.println(height_two);
+        System.out.println(height$12jk);
+        System.out.println(height_);
+        System.out.println(height$);
+
+        //No Reserved Keywords
+        //int class = 22;
+
+        //length
+        int helloWorld = 100;
+        int a=1;
+        int b=2;
+        int c=3;
+        //CamelCase
+        System.out.println(helloWorld);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        //constants
+        int Days_In_Year = 365;
+        System.out.println(Days_In_Year);
+
+
     }
 }
