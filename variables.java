10c1b3fb8bc6a48fd72170dc33ed8b62fb30d9d4
Santosh <spant671@gmail.com>
2026-04-18 10:35:01 +0530

    new commit

---
+++ variables.java
@@ -0,0 +1,23 @@
+public class variables {
+    static void main(){
+
+        //create //declare
+        int age;
+        //System.out.println(age);
+        //assign value
+        age = 10;
+        System.out.println(age);
+
+        //define or initialize
+        int totalMarks = 20;
+        System.out.println(totalMarks);
+
+        //Case Sensitive
+        int weight = 80;
+        int WEIGHT = 90 ;
+        System.out.println("weight " +  weight);
+        System.out.println("WEIGHT " +  WEIGHT);
+
+        
+    }
+}
