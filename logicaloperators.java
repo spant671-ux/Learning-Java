9ea1f5f77a099fb45f3f018799a3512584dc31e2
Santosh <spant671@gmail.com>
2026-04-21 21:38:57 +0530

    commited

---
+++ logicaloperators.java
@@ -0,0 +1,11 @@
+public class logicaloperators {
+    public static void main(String[] args) {
+
+        boolean completedDSA = true;
+        boolean completedCore = false;
+
+        System.out.println(completedDSA && completedCore);
+        System.out.println(completedDSA || completedCore);
+        System.out.println(!completedCore);
+    }
+}
