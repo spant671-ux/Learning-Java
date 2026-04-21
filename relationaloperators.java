ee7d611667235b3ef5988a470f3435fde9e1fb9d
Santosh <spant671@gmail.com>
2026-04-21 21:37:43 +0530

    commited

---
+++ relationaloperators.java
@@ -0,0 +1,14 @@
+public class relationaloperators {
+    public static void main(String[] args) {
+
+        int currentStreak = 45;
+        int targetStreak = 50;
+
+        System.out.println(currentStreak == targetStreak);
+        System.out.println(currentStreak != targetStreak);
+        System.out.println(currentStreak > targetStreak);
+        System.out.println(currentStreak < targetStreak);
+        System.out.println(currentStreak >= targetStreak);
+        System.out.println(currentStreak <= targetStreak);
+    }
+}
