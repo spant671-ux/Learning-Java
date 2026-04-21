53dd497cd15b14350ad0a02b4fa9a949a06841ad
Santosh <spant671@gmail.com>
2026-04-21 12:57:04 +0530

    commited

--- datatypes.java
+++ datatypes.java
@@ -13,5 +13,23 @@ public class datatypes {
         System.out.println(num3);
         System.out.println(num4);
     
+        //floating point DT - float, double
+
+        float num5= 3.14f;
+        System.out.println(num5);
+
+        double num6 = 3.1415926535897932384626433832795;
+        System.out.println(num6);
+
+        //other - char, boolean
+
+        boolean eligibleToVote = true;
+        System.out.println("Am I eligible to vote? " + eligibleToVote);
+
+        char firstCharacter = 'A';
+        System.out.println("My First Character is: " + firstCharacter);
+        System.out.println("My First Character is: " + (char)(firstCharacter+2));
+    
+
     }
 }
