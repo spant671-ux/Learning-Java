de960746a5710252b176670527c1da4bbd45f947
Santosh <spant671@gmail.com>
2026-04-21 12:45:15 +0530

    commited

--- datatypes.java
+++ datatypes.java
@@ -5,6 +5,13 @@ public class datatypes {
 
         byte num1 = 100;
         System.out.println(num1);
+
+        short num2 = 1000;
+        long num3 = 100000;
+        int num4 = 83293;
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
     
     }
 }
