5767634c9ab247a139295843d9f42f181afc44dd
Santosh <spant671@gmail.com>
2026-04-21 21:46:38 +0530

    commited

---
+++ operators/unaryoperators.java
@@ -0,0 +1,14 @@
+
+public class unaryoperators {
+    public static void main(String[] args) {
+
+        int activeUsers = 100;
+
+        int prefix = ++activeUsers;
+        int postfix = activeUsers++;
+
+        System.out.println(prefix);
+        System.out.println(postfix);
+        System.out.println(activeUsers);
+    }
+}
