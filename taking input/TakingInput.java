fdf58558d7b42aeb2918bed8a698fa8867f8a14f
Santosh <spant671@gmail.com>
2026-04-29 15:46:16 +0530

    commited

--- taking input/TakingInput.java
+++ taking input/TakingInput.java
@@ -1,5 +1,15 @@
-public class TakingInput {
+import java.util.Scanner;
+
+public class takinginput {
     static void main() {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value for firstnum: ");
+        int firstnum = sc.nextInt();
+        System.out.print("Enter the value for secondnum: ");
+        int secondnum = sc.nextInt();
+        int sum = firstnum + secondnum;
+        System.out.println("The sum of " + firstnum + " and " + secondnum + " is: " + sum);
     }
+
+    
 }
