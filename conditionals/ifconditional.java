71b14669a0104b182c9e264a866a2da320bcd476
Santosh <spant671@gmail.com>
2026-05-02 13:35:25 +0530

    commited

---
+++ conditionals/ifconditional.java
@@ -0,0 +1,16 @@
+public class ifconditional {
+    static void main() {
+        int dailyPractice = 12;
+
+        if (dailyPractice >=10){
+            System.out.println("Good Conistency");
+        }
+
+        int age = 10;
+
+        if (age >=18){
+            System.out.println("You are eligible to vote");
+        }
+    }
+    
+}
---
+++ conditionals/ifelse.java
@@ -0,0 +1,11 @@
+public class ifelse {
+    static void main() {
+        int score = 42;
+
+        if(score>=50){
+            System.out.println("Pass");
+        }else{
+            System.out.println("Fail");
+        }
+    }
+}
