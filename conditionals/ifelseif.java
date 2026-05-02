055595f14e2f83f2e45f50d39d338ff55e408821
Santosh <spant671@gmail.com>
2026-05-02 13:43:01 +0530

    commited

---
+++ conditionals/ifelseif.java
@@ -0,0 +1,19 @@
+public class ifelseif {
+    static void main() {
+
+        int accuracy = 78;
+
+        if(accuracy>=90){
+            System.out.println("Excellent");
+        }else if (accuracy>=75){
+            System.out.println("Good");
+        }else if (accuracy >=60){
+            System.out.println("Average");
+        }else{
+            System.out.println("Needs Improvement");
+        }
+    }
+}
+        
+       
+      
