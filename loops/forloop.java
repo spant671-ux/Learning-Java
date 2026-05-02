a2e4a43b94fe270a9502228b96b439b3077f71d7
Santosh <spant671@gmail.com>
2026-05-02 15:36:58 +0530

    commited

---
+++ loops/forloop.java
@@ -0,0 +1,12 @@
+public class forloop {
+    static void main() {
+        //first loop
+        // for (int i=1; i <=5; i++){
+        //     System.out.println("Value of i: " + i);
+        // }
+        for (int i = 1; i <= 10; i += 2) {
+            System.out.println(i);
+        }
+
+    }
+}
