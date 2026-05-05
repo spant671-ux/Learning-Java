5d6009e4dea242fc23360bd804d03b8ea12b91f8
Santosh <spant671@gmail.com>
2026-05-05 17:22:04 +0530

    commited

--- loops/WhileLoop.java
+++ loops/WhileLoop.java
@@ -1,9 +1,15 @@
-public class WhileLoop{
-    static void main() {
-        int i = 1;
-        while (i <=5){
-            System.out.println(i);
-            i++;
+public class WhileLoop {
+    public static void main(String[] args){
+        
+        /*
+         * Example: A basic while loop.
+         * The loop continues to execute as long as the condition (i <= 10) is true.
+         */
+        int i = 1; // Initialization: Set the starting value
+        
+        while (i <= 10){ // Condition: Check if the value is 10 or less
+            System.out.println(i); // Action: Print the current value
+            i++; // Update: Increment 'i' by 1 to eventually terminate the loop
         }
     }
 }
