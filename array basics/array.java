ea256c5546ddb93058168671ba1d07eb8ce7820a
Santosh <spant671@gmail.com>
2026-06-22 22:20:06 +0530

    added arrays

---
+++ array basics/array.java
@@ -0,0 +1,13 @@
+public class array{
+    static void main(){
+
+        //declaration
+        int arr[];
+
+        //allocation
+        arr= new int[5];
+
+        //init
+         int brr[] = {1, 2, 3};
+    }
+}
