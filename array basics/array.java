18980fdc5ee0926584f66d33bf8f55b3fc724b93
Santosh <spant671@gmail.com>
2026-06-23 21:15:54 +0530

    added arrays

--- array basics/array.java
+++ array basics/array.java
@@ -8,6 +8,17 @@ public class array{
         arr= new int[5];
 
         //init
-         int brr[] = {1, 2, 3};
+         int brr[] = {10, 20, 30};
+         int n=brr.length;
+//        System.out.println("Value at '0' index: "+brr[0]);
+//        System.out.println("Value at '1' index: "+brr[1]);
+//        System.out.println("Value at '2' index: "+brr[2]);
+//        for(int index=0; index<=n-1; index++){
+//            System.out.println(brr[index]);
+//        }
+        for(int val: brr){
+            System.out.println(val);
+        }
+
     }
 }
