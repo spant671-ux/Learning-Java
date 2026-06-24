6901e2ecd1c0c220fe437af02b7bb871479fb795
Santosh <spant671@gmail.com>
2026-06-24 17:31:40 +0530

    added practice arrays

--- array basics/practiceArrays.java
+++ array basics/practiceArrays.java
@@ -1,5 +1,17 @@
-public class newd{
+public class practiceArrays {
     static void main(){
+
+        int arr[] = {3, 2, -5, 21, 10};
+        int n= arr.length;
+        int maxValue = arr[0];
+
+        for(int i=0; i<n; i++){
+            if(arr[i] > maxValue){
+                //update maxValue
+                maxValue = arr[i];
+            }
+        }
+        System.out.println(maxValue);
 //        int arr[] = {1, 2, 3, 4, 5};
 //        int n= arr.length;
 //        int sum =0;
@@ -9,13 +21,15 @@ public class newd{
 //        }
 //        System.out.println(sum);
 
-        int arr[] = {2, 3, 10, 20};
-        int ans =1;
-        int n=arr.length;
-        for(int i=0; i<n; i++){
-            int value = arr[i];
-            ans = value* ans;
-        }
-        System.out.println(ans);
+//        int arr[] = {2, 3, 10, 20};
+//        int ans =1;
+//        int n=arr.length;
+//        for(int i=0; i<n; i++){
+//            int value = arr[i];
+//            ans = value* ans;
+//        }
+//        System.out.println(ans);
+
+
     }
 }
