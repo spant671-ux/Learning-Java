e68322e8e2607c1302e2405cc8d185afce7d5ca8
Santosh <spant671@gmail.com>
2026-07-13 13:00:49 +0530

    added 2d arrays

--- array basics/array2d.java
+++ array basics/array2d.java
@@ -12,6 +12,15 @@ public class array2d{
                 {3, 4},
                 {4, 5}
         };
-        System.out.println(brr[0][0]);
+        //System.out.println(brr[3][0]);
+
+        int rowLength= brr.length;
+        int colLength= brr[0].length;
+        for(int rowIndex = 0; rowIndex <= rowLength-1; rowIndex++){
+            for (int colIndex = 0; colIndex <= colLength-1; colIndex++){
+                System.out.print(brr[rowIndex][colIndex]);
+            }
+            System.out.println();
+        }
     }
 }
