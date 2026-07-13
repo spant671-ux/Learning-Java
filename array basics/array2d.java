c47ec391ddf6e5b6b8a610b8e92dfe13e7929b1e
Santosh <spant671@gmail.com>
2026-07-13 14:01:24 +0530

    added 2d arrays

--- array basics/array2d.java
+++ array basics/array2d.java
@@ -14,14 +14,21 @@ public class array2d{
         };
         //System.out.println(brr[3][0]);
 
-        int rowLength= brr.length;
-        //int colLength= brr[0].length;
-        for(int rowIndex = 0; rowIndex <= rowLength-1; rowIndex++){
-            int colLength= brr[rowIndex].length;
-            for (int colIndex = 0; colIndex <= colLength-1; colIndex++){
-                System.out.print(brr[rowIndex][colIndex] + " ");
+//        int rowLength= brr.length;
+//        //int colLength= brr[0].length;
+//        for(int rowIndex = 0; rowIndex <= rowLength-1; rowIndex++){
+//            int colLength= brr[rowIndex].length;
+//            for (int colIndex = 0; colIndex <= colLength-1; colIndex++){
+//                System.out.print(brr[rowIndex][colIndex] + " ");
+//            }
+//            System.out.println();
+//        }
+
+        //traversal in 2d array
+        for(int rowIndex= 0; rowIndex<= brr.length-1; rowIndex++){
+            for(int colIndex=0; colIndex<=brr[rowIndex].length-1; colIndex++){
+                System.out.println(brr[rowIndex][colIndex]);
             }
-            System.out.println();
         }
     }
 }
