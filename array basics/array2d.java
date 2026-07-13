6e2ca6d3546fe1d1f7d183a53a7ee995a8ec1448
Santosh <spant671@gmail.com>
2026-07-13 13:56:37 +0530

    added 2d arrays

--- array basics/array2d.java
+++ array basics/array2d.java
@@ -7,18 +7,19 @@ public class array2d{
         arr= new int[3][4];
         //init
         int[][]brr= {
-                {1, 2},
-                {2, 3},
-                {3, 4},
-                {4, 5}
+                {1,2},
+                {2,3,4,5},
+                {3,4,5,6,7,8},
+                {4}
         };
         //System.out.println(brr[3][0]);
 
         int rowLength= brr.length;
-        int colLength= brr[0].length;
+        //int colLength= brr[0].length;
         for(int rowIndex = 0; rowIndex <= rowLength-1; rowIndex++){
+            int colLength= brr[rowIndex].length;
             for (int colIndex = 0; colIndex <= colLength-1; colIndex++){
-                System.out.print(brr[rowIndex][colIndex]);
+                System.out.print(brr[rowIndex][colIndex] + " ");
             }
             System.out.println();
         }
