ebe36403d5c2ddb6acb7e638ea19b7c8b0df6fd3
Santosh <spant671@gmail.com>
2026-07-13 12:55:07 +0530

    added 2d arrays

---
+++ array basics/array2d.java
@@ -0,0 +1,17 @@
+public class array2d{
+    static void main(){
+
+        //declaration
+        int[][] arr;
+        //allocation
+        arr= new int[3][4];
+        //init
+        int[][]brr= {
+                {1, 2},
+                {2, 3},
+                {3, 4},
+                {4, 5}
+        };
+        System.out.println(brr[0][0]);
+    }
+}
