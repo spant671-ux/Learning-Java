c8cf193235bcdac3abe63983cb092cce75afbd9c
Santosh <spant671@gmail.com>
2026-07-13 14:14:31 +0530

    added 2d arrays

--- array basics/array2d.java
+++ array basics/array2d.java
@@ -1,17 +1,41 @@
+import java.util.Scanner;
 public class array2d{
     static void main(){
 
-        //declaration
-        int[][] arr;
-        //allocation
-        arr= new int[3][4];
-        //init
-        int[][]brr= {
-                {1,2},
-                {2,3,4,5},
-                {3,4,5,6,7,8},
-                {4}
-        };
+        int arr[][] = new int[3][4];
+
+        Scanner sc = new Scanner(System.in);
+        //input
+
+        for(int i = 0 ; i<=arr.length-1; i++){
+            for( int j = 0 ; j<=arr[i].length-1; j++){
+                System.out.println("Provide value for row = "+i+ "and value for column = "+j );
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        //print
+
+        for(int rowIndex= 0; rowIndex<= arr.length-1; rowIndex++){
+           for(int colIndex=0; colIndex<=arr[rowIndex].length-1; colIndex++){
+               System.out.print(arr[rowIndex][colIndex] + " ") ;
+            }
+            System.out.println();
+        }
+
+
+
+//        //declaration
+//        int[][] arr;
+//        //allocation
+//        arr= new int[3][4];
+//        //init
+//        int[][]brr= {
+//                {1,2},
+//                {2,3,4,5},
+//                {3,4,5,6,7,8},
+//                {4}
+//        };
         //System.out.println(brr[3][0]);
 
 //        int rowLength= brr.length;
@@ -25,11 +49,12 @@ public class array2d{
 //        }
 
         //traversal in 2d array
-        for(int rowIndex= 0; rowIndex<= brr.length-1; rowIndex++){
-            for(int colIndex=0; colIndex<=brr[rowIndex].length-1; colIndex++){
-                System.out.print(brr[rowIndex][colIndex] + " ") ;
-            }
-            System.out.println();
-        }
+//        for(int rowIndex= 0; rowIndex<= brr.length-1; rowIndex++){
+//            for(int colIndex=0; colIndex<=brr[rowIndex].length-1; colIndex++){
+//                System.out.print(brr[rowIndex][colIndex] + " ") ;
+//            }
+//            System.out.println();
+//        }
+
     }
 }
